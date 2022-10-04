4f7b0ba90a989069b110fa324b201a7959140c00
Nishaphadtare <nishaphadtare03@gmail.com>
2022-10-04 13:32:25 +0530

    First program execution in JS
    First program execution in JS

---
+++ assignment.js
@@ -0,0 +1,6 @@
+console.log("Nisha Kakasaheb Phadtare");
+console.log("a) Hobbies   :-   1.traveling      2.drawing    3.cooking");
+console.log("b) strength  :-   1.I am quick learner   2.I am a good team player   3.I am also good at  multitasking");
+console.warn("c) weakness  :-   1.bad handwriting      2.I concentrate on one thing at a time   3.my weakness is i am not good at designing yes");
+console.log(30+50);
+console.log(49*479);
---
+++ demo.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+console.log("My name is nisha phadtare");
+console.info("This is nifo");
+console.warn("i am not that bad girl");
+console.error("Opps! failed to executed");
